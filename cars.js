a781f96709c4ae836e613d40cd82970af8e0fc2c
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-09-09 12:40:09 +0100

    created cars file and included cars array

---
+++ cars.js
@@ -0,0 +1,36 @@
+const cars = [
+  {
+    id: 1,
+    make: "honda",
+    model: "accord",
+    year: 86,
+    doors: 4,
+    color: "grey",
+  },
+  {
+    id: 2,
+    make: "mazda",
+    model: "626",
+    year: 2002,
+    doors: 4,
+    color: "black",
+  },
+  {
+    id: 3,
+    make: "honda",
+    model: "CRV",
+    year: 2002,
+    doors: 4,
+    color: "black",
+  },
+  {
+    id: 4,
+    make: "toyota",
+    model: "Corolla",
+    year: 2005,
+    doors: 4,
+    color: "blue",
+  },
+];
+
+module.exports = { cars };
